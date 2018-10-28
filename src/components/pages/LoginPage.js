b50c90efc522e9224d9073e561e4a9bb9a90cec8
Konrad Baczyński <damrod330@gmail.com>
2018-10-28 02:39:36 +0200

    full screen image loginPage

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -4,37 +4,35 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 
+import './LoginPage.css';
+
 class LoginPage extends Component {
+
     render() {
         return (
-            <Grid
-                container
-                direction="row"
-                justify="center"
-                alignItems="center">
-                <Paper>
-                    <Grid
-                        container>
-                        <Grid item xs={12}>
-                            <TextField
-                                id="standard-name"
-                                label="Nazwa użytkownika"
-                                type="text"
-                                margin="normal"
-                            />
-                        </Grid>
+            <div className="bg-mask">
+                <div className="frosted-glass login-card">
+                    <div className="card">
+                        <TextField
+                            className="white-text"
+                            id="standard-name"
+                            label="Nazwa użytkownika"
+                            type="text"
+                            margin="normal"
+                        />
 
-                        <Grid item xs={12}>
-                            <TextField
-                                id="password"
-                                type="password"
-                                label="Hasło"
-                                margin="normal"
-                            />
-                        </Grid>
-                    </Grid>
-                </Paper>
-            </Grid>
+                        <TextField
+                            className="white-text"
+                            id="password"
+                            type="password"
+                            label="Hasło"
+                            margin="normal"
+                        />
+
+                        <Button variant="contained" color="primary"> Zaloguj się</Button>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
