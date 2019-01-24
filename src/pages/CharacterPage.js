2116b9d87b511512865706b9a10f17a2e5a77b2f
Konrad <konrad.baczynski@almar.pl>
2019-01-24 09:45:26 +0100

    CharacterPage updated

--- src/pages/CharacterPage.js
+++ src/pages/CharacterPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import HeroInfo from '../components/fragments/HeroInfo'
+import HeroInfo from '../components/fragments/HeroInfo-old'
 import HeroTraits from '../components/fragments/HeroTraits'
 import Grid from '@material-ui/core/Grid';
 import axios from '../axios';
@@ -7,6 +7,7 @@ import SideNav from '../components/SideNav';
 import '../styles/main.css';
 import '../styles/CharacterPage.css';
 import HeroAbilities from '../components/fragments/HeroAbilities';
+import HeroSkills from '../components/fragments/HeroSkills';
 
 
 class CharacterPage extends Component {
@@ -31,7 +32,7 @@ class CharacterPage extends Component {
                     <div className="container">
                         <Grid container spacing={16}>
                             <Grid item xs={12} sm={6} md={4}>
-                                <HeroInfo />
+                                {/* <HeroInfo /> */}
                             </Grid>
                             <Grid item xs={6} sm={6} md={2}>
                                 <HeroTraits traits={this.state.data.traits} 
@@ -40,6 +41,9 @@ class CharacterPage extends Component {
                             </Grid>
                             <Grid item xs={12} sm={6} md={3}>
                                 <HeroAbilities abilities={this.state.data.abilities} characterId={this.state.data.id}/>
+                            </Grid>
+                            <Grid item xs={12} sm={6} md={3}>
+                                <HeroSkills skills={this.state.data.skills} characterId={this.state.data.id}/>
                             </Grid>
                         </Grid>
                     </div> : null}
